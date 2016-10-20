09faefa85e1f7e10c67dc09f7c35c63e10b047d3
DavidTrampe <dtamm22@gmail.com>
2016-10-20 13:05:58 +0200

    Första view

--- NumberGame/src/com/hule/main/View.java
+++ NumberGame/src/com/hule/main/View.java
@@ -1,5 +1,46 @@
-package com.hule.main;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package numbergame;
 
-public class View {
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
 
+/**
+ *
+ * @author Hule
+ */
+public class View extends JFrame{
+    Model model;
+    int size;
+    int row, col;
+    JButton knapp[][];
+    JPanel panGame;
+    
+    public View(Model model){
+        //Konstruktor och model ska anropa denna metoden när ett drag görs
+        this.model = model;
+        this.size = model.boardSize();
+        panGame = new JPanel(new GridLayout(size, size));
+        Container container = getContentPane();
+        container.setLayout(new BorderLayout());
+        container.add(panGame, "Center");
+        
+        for (int i = 0; i < size; i++){
+            for (int j = 0; j <size; j++){
+                knapp[i][j] = new JButton(Model.getNumber(i, j));
+                panGame.add(knapp[i][j]);
+            }
+        }
+        
+        
+}
+    public void update(){
+        //få in värden och update spelbordet grafiskt
+        
+        
+    }
 }
